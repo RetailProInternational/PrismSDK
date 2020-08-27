9552ffaf06164ebad5f1f578df6e3d6c3f834f4c
akankanala <68252185+akankanala@users.noreply.github.com>
2020-08-27 14:24:53 -0700

    added custom-translations-example

---
+++ UI/custom-translations-example/plugins/customTranslationsTestConfig.js
@@ -0,0 +1,32 @@
+/*
+Copyright © 2020 Retail Pro International, LLC. - All Rights Reserved.
+
+This source code is proprietary and Confidential Information of Retail Pro International, LLC.
+
+Any use of this source code, or any portion thereof, for any purpose whatsoever, is subject to adherence with the 
+applicable Retail Pro Development Partner Software License.  All other use or distribution in any format whatsoever, 
+is unauthorized and subject to appropriate penalties under applicable law. 
+
+For more information please visit www.retailpro.com or email moreinfo@retailpro.com.
+*/
+
+SideButtonsManager.addButton({
+  label: 'custID1',           // Custom Translation ID from \WebClient\locale\en-us-custom.json
+  icon: '',
+  sections: ['register'],
+  handler: [],
+});
+
+SideButtonsManager.addButton({
+  label: '12',                // Core Translation ID from \WebClient\locale\en-us.json
+  icon: '',
+  sections: ['register'],
+  handler: [],
+});
+
+SideButtonsManager.addButton({
+  label: 'Just Text',    // Simple text without Translation ID
+  icon: '',
+  sections: ['register'],
+  handler: [],
+});
